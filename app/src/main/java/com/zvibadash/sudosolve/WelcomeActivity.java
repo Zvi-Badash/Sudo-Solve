b6492463041c382718773a881fc66f01b12ab4f1
Zvi Badash <zvi.badash@gmail.com>
2021-11-23 22:03:10 +0200

    Added a dummy login activity, LoginActivity.java. Moved there from WelcomeActivity.java.

---
+++ app/src/main/java/com/zvibadash/sudosolve/WelcomeActivity.java
@@ -0,0 +1,33 @@
+package com.zvibadash.sudosolve;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.widget.Toast;
+
+public class WelcomeActivity extends AppCompatActivity {
+    CountDownTimer cdt;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+        Toast toast = Toast.makeText(getApplicationContext(), "", Toast.LENGTH_SHORT);
+        cdt = new CountDownTimer(3_000, 1_000) {
+            @Override
+            public void onTick(long l) {
+                toast.cancel();
+                toast.setText("...");
+                toast.show();
+            }
+
+            @Override
+            public void onFinish() {
+                startActivity(new Intent(WelcomeActivity.this, LoginActivity.class));
+            }
+        }.start();
+    }
+}
