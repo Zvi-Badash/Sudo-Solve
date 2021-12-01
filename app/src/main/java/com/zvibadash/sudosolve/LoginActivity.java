bf0d948a9fe68ebb903c707252d501e20a020194
Zvi Badash <zvi.badash@gmail.com>
2021-12-01 13:38:00 +0200

    Added the contract and helper classes.

--- app/src/main/java/com/zvibadash/sudosolve/LoginActivity.java
+++ app/src/main/java/com/zvibadash/sudosolve/LoginActivity.java
@@ -2,13 +2,33 @@ package com.zvibadash.sudosolve;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+    SSDBHelper dbHelper;
+    SQLiteDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        dbHelper = new SSDBHelper(getApplicationContext());
+        db = dbHelper.getWritableDatabase();
+
+        Button btLogin = findViewById(R.id.btLogin);
+        btLogin.setOnClickListener(view -> {
+            EditText etUsername = findViewById(R.id.etUsername);
+
+        });
+    }
+
+    private Boolean isValidUsername(String username) {
+        // 2 < username's length < 16
+        return username.length() < 16 && username.length() > 2;
     }
 }
---
+++ app/src/main/java/com/zvibadash/sudosolve/SSDBContract.java
@@ -0,0 +1,46 @@
+package com.zvibadash.sudosolve;
+
+
+import android.provider.BaseColumns;
+
+public final class SSDBContract {
+    // To prevent someone from accidentally instantiating the contract class,
+    // make the constructor private.
+    private SSDBContract() {}
+
+    /* Inner class that defines the table contents */
+    public static class User implements BaseColumns {
+        // That is the table's name.
+        public static final String TABLE_NAME = "user";
+
+        // Here, the schema is actually detailed.
+        public static final String USERNAME = "username";
+    }
+
+    public static class Session implements BaseColumns {
+        // That is the table's name.
+        public static final String TABLE_NAME = "session";
+
+        // Here, the schema is actually detailed.
+        public static final String SESSION_ID = "sess_id";
+        public static final String SESSION_CONTENTS = "sess_cont";
+    }
+
+    public static class Statistics implements BaseColumns {
+        // That is the table's name.
+        public static final String TABLE_NAME = "stat";
+
+        // Here, the schema is actually detailed.
+        public static final String USERNAME = User.USERNAME;
+        public static final String STATISTICS_CONTENTS = "stat_cont";
+    }
+
+    public static class LastSessionForUser implements BaseColumns {
+        // That is the table's name.
+        public static final String TABLE_NAME = "last_sess";
+
+        // Here, the schema is actually detailed.
+        public static final String USERNAME = User.USERNAME;
+        public static final String SESSION_ID = Session.SESSION_ID;
+    }
+}
